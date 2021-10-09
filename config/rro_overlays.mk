c20abbed238fc86756136f9cdd5c769297b4ea3a
Dyneteve <dyneteve@hentaios.com>
2021-10-09 15:02:42 +0200

    vendor: overlay: Update from SP1A.210812.016
    Change-Id: Idd547f7c5d0a2cf4509d3254346f854f272232ea
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

---
+++ config/rro_overlays.mk
@@ -0,0 +1,16 @@
+# Google Overlays
+PRODUCT_PACKAGES += \
+    CaptivePortalLoginOverlay \
+    CellBroadcastServiceOverlay \
+    GoogleConfigOverlay \
+    GooglePermissionControllerOverlay \
+    GoogleWebViewOverlay \
+    ManagedProvisioningPixelOverlay \
+    ManagedProvisioningPixelOverlay \
+    PixelConfigOverlay2018 \
+    PixelConfigOverlay2019 \
+    PixelConfigOverlay2019Midyear \
+    PixelConfigOverlayCommon \
+    PixelSetupWizardOverlayAosp \
+    PixelTetheringOverlay \
+    SettingsGoogleOverlayAosp
